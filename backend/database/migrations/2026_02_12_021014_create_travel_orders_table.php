76614fe9392ead58a8d6748c1b180bdd2597f0a3
tiago <tiagogarcia@somultas.com>
2026-02-11 23:12:56 -0300

    chore: update file permissions for backend files and directories

---
+++ backend/database/migrations/2026_02_12_021014_create_travel_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+ public function up(): void
+{
+    Schema::create('travel_orders', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        $table->string('requester_name');
+        $table->string('destination');
+        $table->date('departure_date');
+        $table->date('return_date');
+        $table->string('status')->default('requested');
+        $table->timestamps();
+    });
+}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travel_orders');
+    }
+};
