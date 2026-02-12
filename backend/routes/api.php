0ba6ec6dabecc02d1be4212cd067511d8bac1ae6
Tiago Abranges <tiagogarcia@somultas.com>
2026-02-12 11:23:47 -0300

    feat: define API routes for TravelOrderController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TravelOrderController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/orders', [TravelOrderController::class, 'index']);
+Route::post('/orders', [TravelOrderController::class, 'store']);
+Route::get('/orders/{id}', [TravelOrderController::class, 'show']);
+Route::patch('/orders/{id}/status', [TravelOrderController::class, 'updateStatus']);
