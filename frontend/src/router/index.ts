dc160164e2535467af550a5dd3c09a7231cc5196
tiago <tiagogarcia@somultas.com>
2026-02-13 15:49:13 -0300

    fix: correct import case for Login and Dashboard views

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/Login.vue";
-import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/login.vue";
+import Dashboard from "../views/dashboard.vue";
 
 const routes = [
   {
